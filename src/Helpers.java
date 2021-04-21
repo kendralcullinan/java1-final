dd3b713995039b22b3090a7acb601c46790061dd
Kendra Cullinan <kendralcullinan@gmail.com>
2021-04-21 13:26:04 -0500

    Added getters/setters

--- src/Helpers.java
+++ src/Helpers.java
@@ -1,7 +1,17 @@
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.DateTimeException;
+import java.util.Scanner;
 public class Helpers {
+
+    public static void testData(Scanner scanner){
+        Kitten k1 = new Kitten();
+        Kitten k2 = new Kitten("Snickers", "Girl", "2021-04-22", 5, true);
+        System.out.print(k1);
+        System.out.println(k2);
+        UIUtility.pressEntertoContinue(scanner);
+    }
+
     public static LocalDate convertStrToDate(String date){
         DateTimeFormatter formatterInput = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate userDate; 
@@ -11,6 +21,5 @@ public class Helpers {
             userDate = LocalDate.now();
         }
         return userDate;
-
     }
 }
--- src/Kitten.java
+++ src/Kitten.java
@@ -1,28 +1,71 @@
-
+import java.time.LocalDate;
 public class Kitten implements Comparable <Kitten> {
     private String name;
-    private String color;
+    private String gender;
     private LocalDate birthDate;
     private int numKittens;
     private boolean fed;
     
     public Kitten(){
         this.name = "";
-        this.color = "";
+        this.gender = "";
         this.birthDate = LocalDate.now();
         this.numKittens = 0;
         this.fed  = false;
     }
     
-    public Kitten(String name, String color, String birthDate, int numKittens, boolean fed){
+    public Kitten(String name, String gender, String birthDate, int numKittens, boolean fed){
         this.name = name;
-        this.color = color;
+        this.gender = gender;
         this.birthDate = convertStrToDate(birthDate);
         this.numKittens = numKittens;
         this.fed  = fed;
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(String birthDate) {
+        this.birthDate = Helpers.convertStrToDate(birthDate);
+    }
+
+    public int getNumKittens() {
+        return numKittens;
+    }
+    public void setNumKittens(int numKittens) {
+        this.numKittens = numKittens;
+    }
+
+    public boolean isFed() {
+        return fed;
+    }
+    public void setFed(boolean fed) {
+        this.fed = fed;
+    }
+    
+    public String toString(){
+        String result = "";
+        result += "Name: " + name + ", " + gender + birthDate;
+        return result;
+    }
+
+    @Override
     public int compareTo(Kitten other){
         int result = this.name.compareTo(other.name);
         if(result == 0){
@@ -30,4 +73,6 @@ public class Kitten implements Comparable <Kitten> {
         }
         return result;
     }
+
+    
 }
